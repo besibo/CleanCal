f095a752d7f7056d5ddc0827c905fa77975df6c7
Benoit Simon-Bouhet <besibo@gmail.com>
2022-05-11 09:20:47 +0200

    Update calendar

--- ical.R
+++ ical.R
@@ -14,34 +14,3 @@ ic_read("https://apps.univ-lr.fr/serveur-planning/wa/iCalendarOccupations?login=
          SUMMARY = str_remove(SUMMARY, "^C\\d{1}-.{14}"),
          DESCRIPTION = str_remove(DESCRIPTION, "^C\\d{1}-.{14}")) %>% 
   ic_write(file = "~/Desktop/CleanCal/Data/res.ics")
-
-
-
-
-# a <- read.table("https://apps.univ-lr.fr/serveur-planning/wa/iCalendarOccupations?login=bsimonbo")
-# 
-# 
-# ic_read("https://besibo.github.io/CleanCal/data/res.ics")
-
-# # 
-# tmp <- ic_read("https://besibo.github.io/CleanCal/data/res.ics")
-# tmp %>%
-#   filter(str_detect(DESCRIPTION, "rentrée"))
-# # 
-# tmp2 <- ic_read("https://apps.univ-lr.fr/serveur-planning/wa/iCalendarOccupations?login=bsimonbo")
-# tmp2 %>%
-#   filter(str_detect(DESCRIPTION, "rentrée")) %>% view()
-# 
-# tmp3 <- ic_read("~/Desktop/CleanCal/Data/res.ics")
-# tmp3 %>%
-#   filter(str_detect(DESCRIPTION, "SPE"))
-# 
-# 
-# tmp4 <- ic_read("https://apps.univ-lr.fr/serveur-planning/wa/iCalendarOccupations?login=bsimonbo") %>% 
-#   mutate(SUMMARY = str_remove(SUMMARY, "^B.Simon-bouhet  - "),
-#          DESCRIPTION = str_remove(DESCRIPTION, "B.Simon-bouhet  - "),
-#          SUMMARY = str_remove(SUMMARY, "^C\\d{1}-.{14}"),
-#          DESCRIPTION = str_remove(DESCRIPTION, "^C\\d{1}-.{14}"))
-# 
-# tmp4 %>% 
-#   filter(str_detect(DESCRIPTION, "SPE"))
