cc088a9b7882a627bdbffd68a2c0881a1fb37320
Benoit Simon-Bouhet <besibo@gmail.com>
2022-05-11 11:27:04 +0200

    Update calendar

--- Run.sh
+++ Run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export PATH="/usr/local/bin:$PATH"
-/usr/local/bin/Rscript ical.R
+Rscript ical.R
 # cd ~/Desktop/CleanCal/
-/usr/local/bin/Rscript -e "bookdown::render_book()"
+Rscript -e "bookdown::render_book()"
 git add .
 git commit -m "Update calendar"
 git push origin main
