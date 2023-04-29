ab8f66b2451e6432091ce598e2f3a32928d913b1
Benoit Simon-Bouhet <besibo@gmail.com>
2023-04-29 16:00:02 +0200

    Update calendar

--- ical.R
+++ ical.R
@@ -8,6 +8,13 @@ options(tidyverse.quiet = TRUE)
 library(calendar)
 library(tidyverse)
 
-tmp <- ic_read("https://apps.univ-lr.fr/serveur-planning/wa/iCalendarOccupations?login=bsimonbo") 
-
-ic_write(tmp, file = "/Users/bsimonbo/Documents/TAF/Enseignements/CleanCal/docs/data/res.ics")
+ic_read("https://apps.univ-lr.fr/serveur-planning/wa/iCalendarOccupations?login=bsimonbo") %>% 
+  mutate(SUMMARY = str_remove(SUMMARY, "^B.Simon-bouhet  - "),
+         SUMMARY = str_remove(SUMMARY, "^B. Simon-bouhet  - "),
+         SUMMARY = str_remove(SUMMARY, "^C\\d{1}-.{14}"),
+         SUMMARY = str_remove(SUMMARY, "\\d{3}-\\d{1}-\\d{2}.{3}"),
+         DESCRIPTION = str_remove(DESCRIPTION, "B.Simon-bouhet  - "),
+         DESCRIPTION = str_remove(DESCRIPTION, "B. Simon-bouhet  - "),
+         DESCRIPTION = str_remove(DESCRIPTION, "^C\\d{1}-.{14}"),
+         DESCRIPTION = str_remove(DESCRIPTION, "\\d{3}-\\d{1}-\\d{2}.{3}")) %>% 
+  ic_write(file = "/Users/bsimonbo/Documents/TAF/Enseignements/CleanCal/docs/data/res.ics")
