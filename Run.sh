71af6228e0e1a3e2977d363b28c4c8669019cce7
Benoit Simon-Bouhet <besibo@gmail.com>
2023-04-29 16:15:28 +0200

    Update calendar

--- Run.sh
+++ Run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PATH="/usr/local/bin:$PATH"
 
-Rscript -e "source('ical.R')"
+Rscript ical.R
 cd /Users/bsimonbo/Documents/TAF/Enseignements/CleanCal/
 # Rscript -e "bookdown::render_book()"
 
