563af2d8bbeb11e39f91946d7d0e7119e5216f53
Benoit Simon-Bouhet <besibo@gmail.com>
2024-09-12 15:59:12 +0200

    Update calendar

--- ical.R
+++ ical.R
@@ -11,6 +11,7 @@ library(tidyverse)
 ic_read("https://apps.univ-lr.fr/cgi-bin/WebObjects/ServeurPlanning.woa/wa/iCalendarOccupations?login=bsimonbo") %>% 
   mutate(SUMMARY = str_remove(SUMMARY, "^B.Simon-bouhet  - "),
          SUMMARY = str_remove(SUMMARY, "^B. Simon-bouhet  - "),
+         SUMMARY = str_remove(SUMMARY, "; SIMON-BOUHET Benoit [EDT]"),
          SUMMARY = str_remove(SUMMARY, "^C\\d{1}-.{14}"),
          SUMMARY = str_remove(SUMMARY, "\\d{3}-\\d{1}-\\d{2}.{3}"),
          DESCRIPTION = str_remove(DESCRIPTION, "B.Simon-bouhet  - "),
