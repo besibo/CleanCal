d57c09e89b2ed01f596151b9614f9cc96f7deaf6
Benoit Simon-Bouhet <besibo@gmail.com>
2024-09-24 09:56:08 +0200

    Update calendar

--- ical.R
+++ ical.R
@@ -9,16 +9,16 @@ library(calendar)
 library(tidyverse)
 
 ic_read("https://apps.univ-lr.fr/cgi-bin/WebObjects/ServeurPlanning.woa/wa/iCalendarOccupations?login=bsimonbo") %>% 
-  mutate(SUMMARY = str_remove(SUMMARY, "^B.Simon-bouhet  - "),
-         SUMMARY = str_remove(SUMMARY, "^B. Simon-bouhet  - "),
-         SUMMARY = str_remove(SUMMARY, "\\\\\\; Outils pour l'étude et la compréhension du vivant "),
-         SUMMARY = str_remove(SUMMARY, "\\\\\\; SIMON-BOUHET Benoit \\[EDT\\]"),
-         SUMMARY = str_remove(SUMMARY, "^C\\d{1}-.{14}"),
-         SUMMARY = str_remove(SUMMARY, "\\d{3}-\\d{1}-\\d{2}.{3}"),
-         DESCRIPTION = str_remove(DESCRIPTION, "B.Simon-bouhet  - "),
-         DESCRIPTION = str_remove(DESCRIPTION, "^C\\d{1}-.{14}"),
-         DESCRIPTION = str_remove(DESCRIPTION, "B. Simon-bouhet  - "),
-         DESCRIPTION = str_remove(DESCRIPTION, "\\\\\\; Outils pour l'étude et la compréhension du vivant "),
-         DESCRIPTION = str_remove(DESCRIPTION, "\\\\\\; SIMON-BOUHET Benoit \\[EDT\\]"),
-         DESCRIPTION = str_remove(DESCRIPTION, "\\d{3}-\\d{1}-\\d{2}.{3}")) %>%
+  mutate(SUMMARY = str_remove_all(SUMMARY, "^B.Simon-bouhet  - "),
+         SUMMARY = str_remove_all(SUMMARY, "^B. Simon-bouhet  - "),
+         SUMMARY = str_remove_all(SUMMARY, "\\\\\\; Outils pour l'étude et la compréhension du vivant "),
+         SUMMARY = str_remove_all(SUMMARY, "\\\\\\; SIMON-BOUHET Benoit \\[EDT\\]"),
+         SUMMARY = str_remove_all(SUMMARY, "^C\\d{1}-.{14}"),
+         SUMMARY = str_remove_all(SUMMARY, "\\d{3}-\\d{1}-\\d{2}.{3}"),
+         DESCRIPTION = str_remove_all(DESCRIPTION, "B.Simon-bouhet  - "),
+         DESCRIPTION = str_remove_all(DESCRIPTION, "^C\\d{1}-.{14}"),
+         DESCRIPTION = str_remove_all(DESCRIPTION, "B. Simon-bouhet  - "),
+         DESCRIPTION = str_remove_all(DESCRIPTION, "\\\\\\; Outils pour l'étude et la compréhension du vivant "),
+         DESCRIPTION = str_remove_all(DESCRIPTION, "\\\\\\; SIMON-BOUHET Benoit \\[EDT\\]"),
+         DESCRIPTION = str_remove_all(DESCRIPTION, "\\d{3}-\\d{1}-\\d{2}.{3}")) %>%
   ic_write(file = "/Users/bsimonbo/Documents/TAF/Enseignements/CleanCal/docs/data/res.ics")
